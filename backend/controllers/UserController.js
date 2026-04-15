269772a610cdcd377207f6563fc21658b9e9db1d
JoaopedroRib06 <joaopedrorbj9@gmail.com>
2026-04-15 12:56:45 -0300

    Aula 0415 -  POST API Register User Criação de Dominio

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,54 @@
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 module.exports = class UserController {
     static async register(req, res) {
-        res.json('Olá Get Pet')
+      const{name,email,phone,password,confirmpassword} = req.body
+      if(!name){
+        res.status(422).json({message: 'Nome é obrigatório'})
+        return
+      }
+      if(!email){
+        res.status(422).json({message:'Email é obrigatório'})
+        return
+      }
+      if(!phone){
+        res.status(422).json({message: 'Telefone é obrigatório'})
+        return
+      }
+      if(!password){
+        res.status(422).json({message: 'Senha é obrigatória'})
+        return
+      }
+      if(!confirmpassword){
+        res.status(422).json({message: 'Confirmar a senha é obrigatório'})
+        return
+      }
+      if(password !== confirmpassword){
+        res.status(422).json({message:'As senhas não batem'})
+        return
+      }
+      const userExists = await User.findOne({email : email})
+
+      if(userExists){
+        res.status(422).json({message: 'O usuário já existe em nossos registros'})
+        return
+      }
+
+      const salt = await bcrypt.genSalt(12)
+      const passwordHash = await bcrypt.hash(password, salt)
+
+      const user = new User({
+        name,
+        email,
+        phone,
+        password:passwordHash
+      })
+
+      try {
+        const newUser = await user.save()
+        res.status(201).json({message:'Usuário criado com sucesso', newUser})
+      } catch (error) {
+        res.status(503).json({message:error})
+      }
     }
 }
